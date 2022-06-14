2b6c5b98a2aa78597262597f9dffb3b36ac93190
강주협 <kangjuhyup@gangjuhyeobs-MacBook-Air.local>
2022-06-14 16:17:28 +0900

    upgrade solidity compiler

--- Creator/BSC/BSC_SubscriptionCreator.sol
+++ Creator/BSC/BSC_SubscriptionCreator.sol
@@ -7,22 +7,23 @@ import "../../Commander/BSC/BSC_Commander.sol";
 
 contract BSC_SubscriptionCreator is Subscription, Commander {
     uint8 private swapCount;
-    uint16[2] private tokenIdx = [101,550];
+    uint16[2] private tokenIdx = [550,101];
     uint8 private nowSwap;
     event createSubscriptionEvent( address indexed pack, uint256 createNum, PackInfo packInfo );  // 0: pack indexed, 1 : craeteTime , 2 : packInfo
     event setSwapCountEvent( address indexed owner, uint8 beforeCnt, uint8 afterCnt);
     function createSubscription( PackInfo calldata _packInfo , uint256 _createNum ) external payable 
     {
         require(_packInfo.total <= 1000 ,"C05");
-        uint8 i = 0;
-        if(swapCount>0) {
-            nowSwap++;
-            if(swapCount == nowSwap) {
-                nowSwap = 0;
-                i = 1;
-            }
-        }
-        _swap(tokenIdx[i],msg.sender,msg.value);
+        // uint8 i = 0;
+        // if(swapCount>0) {
+        //     nowSwap++;
+        //     if(swapCount == nowSwap) {
+        //         nowSwap = 0;
+        //         i = 1;
+        //     }
+        // }
+        // _swap(tokenIdx[i],msg.sender,msg.value);
+        _swap(101,msg.sender,msg.value);
         SubscriptionPack pers = new SubscriptionPack( _packInfo, msg.sender );
         emit createSubscriptionEvent( address(pers), _createNum , _packInfo );
     }
--- Creator/BSC/BSC_TikcetCreator.sol
+++ Creator/BSC/BSC_TikcetCreator.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GNU LGPLv3
-pragma solidity >= 0.7.0;
+pragma solidity >= 0.8.0;
 pragma experimental ABIEncoderV2;
 
 import "../../Pack/TicketPack.sol";
@@ -17,16 +17,16 @@ contract BSC_TicketCreator is Ticket,Commander {
     function createTicket(  PackInfo calldata _packInfo , uint256 _createNum   ) external payable 
     {
         require(_packInfo.total <= 1000 ,"C05");
-        uint8 i = 0;
-        if(swapCount>0) {
-            nowSwap++;
-            if(swapCount == nowSwap) {
-                nowSwap = 0;
-                i = 1;
-            }
-        }
-        _swap(tokenIdx[i],msg.sender,msg.value);
-        
+        // uint8 i = 0;
+        // if(swapCount>0) {
+        //     nowSwap++;
+        //     if(swapCount == nowSwap) {
+        //         nowSwap = 0;
+        //         i = 1;
+        //     }
+        // }
+        // _swap(tokenIdx[i],msg.sender,msg.value);
+        _swap(101,msg.sender,msg.value);
         TicketPack pers = new TicketPack( _packInfo, msg.sender );
         emit createTicketEvent(address( pers ), _createNum , _packInfo);
     }
