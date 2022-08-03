56a614a682dbafb5c2d7685fa2408377bd294df6
HyunBeom Heo <kenneth.cryptoinc@gmail.com>
2022-08-03 13:50:01 +0900

    fix: CUP-03

--- Creator/BSC/BSC_CouponCreator.sol
+++ Creator/BSC/BSC_CouponCreator.sol
@@ -7,7 +7,7 @@ import "../../Commander/BSC/BSC_Commander.sol";
 contract BSC_CouponCreator is Commander, Coupon {
     event createCouponEvent(address indexed pack, uint256 createNum, PackInfo packInfo); // 0: pack indexed, 1: reference value, 2: PackInfo
 
-    function createCoupon(PackInfo calldata _packInfo, uint256 _createNum) external payable {
+    function createCoupon(PackInfo calldata _packInfo, uint256 _createNum) external {
         require(_packInfo.total <= 3000, "C05");
         CouponPack pers = new CouponPack(_packInfo, msg.sender);
         emit createCouponEvent(address(pers), _createNum, _packInfo);
